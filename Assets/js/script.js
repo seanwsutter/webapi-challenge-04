9b0406dd302e091cf6cf751f10a91ad0fb7c4660
seanwsutter <seanwsutter@gmail.com>
2023-10-04 07:37:48 -0500

    set up

---
+++ Assets/js/script.js
@@ -0,0 +1,8 @@
+/* 
+be thinking about an array of objects might be useful
+use functions to control flow
+no questions/answeers in html
+how can we genearte each new question and aswers on demand
+get quiz to work with one or two
+log
+*/
