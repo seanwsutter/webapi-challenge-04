2014abf187656449b53785166e0b75c6b2b72e2a
seanwsutter <seanwsutter@gmail.com>
2023-10-08 05:46:45 -0500

    wip

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,21 +1,48 @@
-var startBtnEl = document.getElementById("startBtn")
-var questionContainerEl = document.getElementById("questionContainer")
-var questionTitleEl = document.getElementById("questionTitle")
-var optionsButtonEl = document.getElementById("optionBtns")
+
 var quizRules = document.getElementById("quizRules")
+
 var timerTextEl = document.getElementById("timerText")
 var secondsLeft = 180;
+
+var questionContainerEl = document.getElementById("questionContainer")
+var questionTitleEl = document.getElementById("questionTitle")
+
+var startBtnEl = document.getElementById("startBtn")
+var optionsButtonEl = document.getElementById("optionBtns")
+var buttonClassEl = document.querySelector(".buttonClass")
+
 var optionAEl = document.getElementById("optionA")
 var optionBEl = document.getElementById("optionB")
 var optionCEl = document.getElementById("optionC")
 var optionDEl = document.getElementById("optionD")
-var buttonClassEl = document.querySelector(".buttonClass")
-var a;
-var answerA;
 
+/*** questionsArray ***/
+var questionsArray = [
+  {
+    question: "What is the basic scripting language used by web browsers to render pages on the world wide web.",
+    options: [
+      { optionA: "HyperText Markup Language" },
+      { optionB: "Cascade Style Sheets" },
+      { optionC: "Javascript" },
+      { optionD: "World Wide Web" },
+    ],
+    correctAnswer: "HyperText Markup Language"
+  },
+  {
+    question: "When navigating directories in the terminal, what does the command 'ls' do?",
+    options: [
+      { optionA: "it will make a new list file" },
+      { optionB: "creates a new folder" },
+      { optionC: "it will list the contents of current directory" },
+      { optionD: "opens a new terminal window at folder" },
+    ],
+    correctAnswer: "it will list the contents of current directory"
+  },
+]
+
+/*** quizTimer function ***/
 function quizTimer() {
-  // var secondsLeft = 180;   
-  // console.log(secondsLeft)
+  // var secondsLeft = 180;
   var timerInterval = setInterval(function () {
     secondsLeft--;
     timerTextEl.textContent = "Quiz Timer: " + secondsLeft + " seconds left";
@@ -23,125 +50,93 @@ function quizTimer() {
       clearInterval(timerInterval); // Stops timer
     }
   }, 1000);
+  console.log("timer in progress..");
 }
 
-var questionsArray = [
-  {
-    question: "What is the basic scripting language used by web browsers to render pages on the world wide web.",
-    options: [
-      { a: "HyperText Markup Language", correct: true },
-      { b: "Cascade Style Sheets", correct: false },
-      { c: "Javascript", correct: false },
-      { d: "World Wide Web", correct: false },
-    ],
-
-
-  },
-  {
-    question: "When navigating directories in the terminal, what does the command 'ls' do?",
-    options: [
-      { a: "it will make a new list file", correct: false },
-      { b: "creates a new folder", correct: false },
-      { c: "it will list the contents of a directory", correct: true },
-      { d: "opens a new terminal window at folder", correct: false },
-    ],
-    correct: true
-
-  },
-]/* arrays to to hold list of questions with objects */
-buttonClassEl.addEventListener('click', checkAnswer)
-
+/*** function checkAnswer ***/
 function checkAnswer() {
   console.log("checking..");
-  if (questionsArray.options.correct === true) {
+  if (questionsArray[0].correctAnswer) {
     console.log("correct");
+  } else {
 
   }
-
 }
 
+/*** function setQuizQuestions ***/
 function setQuizQuestions() {
   for (var i = 0; i < questionsArray.length; i++) {
-    console.log(questionsArray[i]);
     questionTitleEl.textContent = questionsArray[i].question
 
-    optionAEl.textContent = questionsArray[i].options[0].a
-    optionBEl.textContent = questionsArray[i].options[1].b
-    optionCEl.textContent = questionsArray[i].options[2].c
-    optionDEl.textContent = questionsArray[i].options[3].d
+    optionAEl.textContent = questionsArray[i].options[0].optionA
+    optionBEl.textContent = questionsArray[i].options[1].optionB
+    optionCEl.textContent = questionsArray[i].options[2].optionC
+    optionDEl.textContent = questionsArray[i].options[3].optionD
+
+    console.log(questionsArray[i]);
   }
 }
-// function checkAnswer() {
-/* function startGame */
+
+/*** function startGame ***/
 function startGame() {
-  console.log("checking..");
-
-  startBtnEl.classList.add('hide'); // will use html hide class on Start Quiz button
-  questionContainerEl.classList.remove("hide"); // will remove the hide class from options div 
-  quizRules.classList.add('hide');
-  optionsButtonEl.classList.remove("hide");
+  console.log("checking startGame..");
+  startBtnEl.classList.add('hide'); // hide start button
+  questionContainerEl.classList.remove("hide"); // show question container
+  quizRules.classList.add('hide'); // hide quiz rules
+  optionsButtonEl.classList.remove("hide"); // show button options
   quizTimer();
-  setQuizQuestions();
-
+  setQuizQuestions(); // cycle through question array
 }
 
-// object.addEventListener("click", myScript);
+/*** check function ***/
+buttonClassEl.addEventListener("click", function (click) {
+  for (var i = 0; i < questionsArray.length; i++)
+    if (i === questionsArray.length) {
+      return
 
+    } else {
+      var element = click.target;
+      var correct = questionsArray[i].correctAnswer;
+      i++;
+      console.log(correct);
 
-// optionAEl.addEventListener('click', function (event) {
+      if (element.textContent === correct) {
+        console.log("good");
+        questionTitleEl.textContent = questionsArray.question[0]
+        console.log("wat");
+        // it loops back to load the next question
+        // addQuestion();
 
-// })
+        // scoring happens as the next question loads
+
+        // score++;
+        // console.log("score:" + score);
+        // showCorrectResult();
+        // showScore();
+        // saveScore();
+      } else {
+        console.log("incorrect, -30 seconds");
+        secondsLeft = secondsLeft - 30;
+        // console.log("score:" + score);
+        // showIncorrectResult();
+        // showScore();
+        // if (i === questionsArray.length) {
+        //   // saveScore();
+        // }
+      }
+    }
+});
+
+buttonClassEl.addEventListener('click', checkAnswer)
 startBtnEl.addEventListener('click', startGame, quizTimer);
-// startBtnEl.addEventListener('click', quizTimer);
 
 
-// }
-// var body = document.body;
 
-// var startBtn = document.querySelector(".startButton");
-// // var startBtn = document.createElement("button")
-// var h1El = document.querySelector(".title");
-// var pTag = document.querySelector("#question");
 
-// // var h3El = document.querySelector('timer')
-// // var timer = document.querySelector(".timer")
-// // var timerElement = document.querySelector(".timer-count");
-// var mainContentEl = document.querySelector(".mainContent");
 
-// // var quizQuestion = document.createElement("h2");
-// // var questionList = document.createElement("ul");
+/** comments
 
-// // Selects element by class
-// var timerEl = document.querySelector(".timer");
-
-// // h1El.textContent = "Coding Quiz Challenge";
-// // quizQuestion.textContent = "Clicking the 'Start Quiz' button will start a 5 minute timer!";
-// // startBtn.textContent = "Start Quiz";
-
-// // // quizQuestion.textContent = "What command is used to create a variable where the value stored cannot be changed dynamically?";
-// // q1.textContent = "let";
-// // q2.textContent = "var";
-// // q3.textContent = "function";
-// // q4.textContent = "const";
-
-// // The startGame function is called when the start button is clicked
-// // quizQuestion.textContent = "What command is used to create a variable where the value stored cannot be changed dynamically?";
-// // questionList.textContent = "Select one option";
-
-// function quizTimer() {
-//   var timeLeft = 180;
-//   console.log(timeLeft);
-//   var timeInterval = setInterval(function () {
-//     if (timeLeft > 1) {
-//       timerTextEl.textContent = timeLeft;
-//       timeLeft--;
-//     } else {
-//
-//       clearInterval(timeInterval); // `clearInterval()` to stop the timer
-//     }
-
-/* mini project pseudo
-
+miniproject
 1 Listen for click event listener
 2 start timer - update countdown
   a. eery second - check if game is won or if letters match
@@ -156,12 +151,14 @@ startBtnEl.addEventListener('click', startGame, quizTimer);
     c. if they guess letter, hav they guessed all the letters
       1 end game as a win
 6 once game is over win or loose - display
-
-/* comments
-
+homework
 be thinking about an array of objects might be useful
 use functions to control flow
 no questions/answeers in html
 how can we genearte each new question and aswers on demand
 get quiz to work with one or two question 
-**/
+
+// // var quizQuestion = document.createElement("h2");
+// // var questionList = document.createElement("ul");
+
+*/
