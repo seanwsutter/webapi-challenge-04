bdf72147a565264f0c506d9414d15f21e6c80754
seanwsutter <seanwsutter@gmail.com>
2023-10-11 00:26:00 -0500

    got it working, added save score

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,21 +1,23 @@
-
+/** elements **/
 var quizRules = document.getElementById("quizRules")
-
 var timerTextEl = document.getElementById("timerText")
-var secondsLeft = 180;
-
 var questionContainerEl = document.getElementById("questionContainer")
 var questionTitleEl = document.getElementById("questionTitle")
 
 var startBtnEl = document.getElementById("startBtn")
-var optionsButtonEl = document.getElementById("optionBtns")
 var buttonClassEl = document.getElementById("optionBtns")
-
+var submitBtn = document.querySelector("#submitScoreButton");
+// var optionsButtonEl = document.getElementById("optionBtns")
 var optionAEl = document.getElementById("optionA")
 var optionBEl = document.getElementById("optionB")
 var optionCEl = document.getElementById("optionC")
 var optionDEl = document.getElementById("optionD")
 
+var quizScoreEl = document.getElementById("quizScore");
+var finalScoreEl = document.getElementById("timerText");
+var initialsEl = document.querySelector("#initials");
+
+var secondsLeft = 180;
 var i = 0;
 
 /*** questionsArray ***/
@@ -29,8 +31,17 @@ var questionsArray = [
     question: "When navigating directories in the terminal, what does the command 'ls' do?",
     options: ["it will make a new list file", "creates a new folder", "it will list the contents of current directory", "opens a new terminal window at folder"],
     correctAnswer: "it will list the contents of current directory",
+  },
+  {
+    question: "Which of the following programming languages is a statically- typed language?",
+    options: ["Python", "JavaScript", "Java", "Ruby"],
+    correctAnswer: "Java",
+  },
+  {
+    question: "In JavaScript, which of the following methods is used to add an element to the end of an array?",
+    options: ["push()", "pop()", "shift()", "unshift()"],
+    correctAnswer: "push()",
   }
-
 ]
 
 /*** quizTimer function ***/
@@ -38,106 +49,124 @@ function quizTimer() {
   // var secondsLeft = 180;
   var timerInterval = setInterval(function () {
     secondsLeft--;
-    timerTextEl.textContent = "Quiz Timer: " + secondsLeft + " seconds left";
-    if (secondsLeft === 0) {
+    timerTextEl.innerHTML = "Quiz Timer: " + secondsLeft + " seconds left";
+    if (secondsLeft < 0 || secondsLeft === 0) {
       clearInterval(timerInterval); // Stops timer
+      quizEnd();
     }
   }, 1000);
   console.log("timer in progress..");
 }
-
-/*** function checkAnswer ***/
-function checkAnswer() {
-  console.log("checking..");
-  if (questionsArray[i].correctAnswer) {
-    console.log("correct");
-  } else {
-
-  }
-}
-
 /*** function setQuizQuestions ***/
 function setQuizQuestions() {
   // for (var i = 0; i < questionsArray.length; i++) {
   if (i === questionsArray.length || secondsLeft <= 0) { // out of questions / timer =0
-    return
-
+    quizEnd()
+    // console.log(secondsLeft);
   } else {
     questionTitleEl.textContent = questionsArray[i].question
 
-    optionAEl.textContent = questionsArray[i].options[0]
-    optionBEl.textContent = questionsArray[i].options[1]
-    optionCEl.textContent = questionsArray[i].options[2]
-    optionDEl.textContent = questionsArray[i].options[3]
-
+    optionAEl.innerHTML = questionsArray[i].options[0]
+    optionBEl.innerHTML = questionsArray[i].options[1]
+    optionCEl.innerHTML = questionsArray[i].options[2]
+    optionDEl.innerHTML = questionsArray[i].options[3]
+    // console.log("in setQuizFunction");
   }
-
-  // console.log(questionsArray[i]);
 }
-
 
 /*** function startGame ***/
 function startGame() {
-  console.log("checking startGame..");
+  console.log("starting game..");
   startBtnEl.classList.add('hide'); // hide start button
   questionContainerEl.classList.remove("hide"); // show question container
   quizRules.classList.add('hide'); // hide quiz rules
-  optionsButtonEl.classList.remove("hide"); // show button options
+  buttonClassEl.classList.remove("hide"); // show button options
   quizTimer();
   setQuizQuestions();
 }
 
 /*** check function ***/
-// buttonClassEl.addEventListener("click", function (click) {
 function check(click) {
-  for (var i = 0; i < questionsArray.length; i++)
-    if (i === questionsArray.length) {
-      return
+  // for (var i = 0; i < questionsArray.length; i++)
+  if (i === questionsArray.length || secondsLeft <= 0) { // out of questions / timer =0
+    quizEnd()
+  } else {
+    questionTitleEl.textContent = questionsArray[i].question
 
+    optionAEl.innerHTML = questionsArray[i].options[0]
+    optionBEl.innerHTML = questionsArray[i].options[1]
+    optionCEl.innerHTML = questionsArray[i].options[2]
+    optionDEl.innerHTML = questionsArray[i].options[3]
+    // console.log(" in setQuizFunction ");
+  }
+  if (questionsArray[i].length) {
+
+    return
+  } else {
+    var element = click.target;
+    var correct = questionsArray[i].correctAnswer;
+    i++;
+    // console.log(correct);
+    console.log("current index in questions array", i);
+    if (element.innerHTML == correct) {
+      console.log("correct");
+      // questionTitleEl.textContent = questionsArray[i].question[0]
+      setQuizQuestions()
     } else {
-      var element = click.target;
-      var correct = questionsArray[i].correctAnswer;
-      i++;
-      console.log(correct);
+      console.log("incorrect, -30 seconds. Time remaining:", secondsLeft = secondsLeft - 30)
+      // console.log("time remaining:", + secondsLeft);
+      setQuizQuestions()
 
-      if (element.textContent === correct) {
-        console.log("good");
-        setQuizQuestions()
-        // questionTitleEl.textContent = questionsArray[i].question[0]
-        console.log("wat");
-
-      } else {
-        console.log("incorrect, -30 seconds");
-        secondsLeft = secondsLeft - 30;
-        setQuizQuestions()
-
-
-      }
     }
+  }
 };
 
 buttonClassEl.addEventListener('click', check);
 startBtnEl.addEventListener('click', startGame, quizTimer);
 
+/* quizEnd function */
+function quizEnd() {
+  quizScoreEl.classList.remove('hide');
+  questionContainerEl.classList.add("hide");
+  // console.log(secondsLeft);
+  // timerTextEl = secondsLeft;
+  // quizScoreEl.classList.remove('hide');
 
-// it loops back to load the next question
-// addQuestion();
 
-// scoring happens as the next question loads
+}
 
-// score++;
-// console.log("score:" + score);
-// showCorrectResult();
-// showScore();
-// saveScore();
-// console.log("score:" + score);
-// showIncorrectResult();
-// showScore();
-// if (i === questionsArray.length) {
-//   // saveScore();
-// }
+/* savingScore function */
+function saveScore() {
+  var initials = initialsEl.value.trim();
+  if (initials !== "") {
+    var scores = JSON.parse(window.localStorage.getItem("scores")) || [];
+    var newScore = { score: secondsLeft, initials: initials }
+    scores.push(newScore)
+    window.localStorage.setItem("scores", JSON.stringify(scores));
+    console.log("Saved new score:", newScore);
+  }
+}
+
+submitBtn.onclick = saveScore;
 
 /** comments
+
+// get value of input box
+// make sure value wasn't empty
+// get saved scores from localstorage, or if not any, set to empty array
+// submit button to saveinitials
+// console.log(scores);
+ 
+/* function checkAnswer 
+// function checkAnswer() {
+//   console.log("checking..");
+//   if (questionsArray[i].correctAnswer) {
+//     console.log("correct");
+//   } else {
+
+//   }
+// }
+
 
 miniproject
 1 Listen for click event listener
